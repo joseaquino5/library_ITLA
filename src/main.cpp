dc60cff41645997a6aa5bcd68c38b5d2a173fb8e
José Aquino <jose_aquino5@hotmail.com>
2016-07-25 12:10:36 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,32 +20,5 @@ int main() {
 	}
 	delete library;
 
-//	Libro* libro1 = new Libro();
-//	libro1->setTitulo("libro 1");
-//	Libro* libro2 = new Libro();
-//	libro2->setTitulo("libro 2");
-//	Libro* libro3 = new Libro();
-//	libro3->setTitulo("libro 3");
-//	Libro* libro4 = new Libro();
-//	libro4->setTitulo("libro 4");
-//	Libro* libro5 = new Libro();
-//	libro5->setTitulo("libro 5");
-//
-//	Pila* pila = new Pila();
-//	pila->agregar(libro1);
-//	pila->agregar(libro2);
-//	pila->agregar(libro3);
-//	pila->agregar(libro4);
-//	pila->agregar(libro5);
-//
-//		Libro* iterator = pila->extraer();
-//
-//		while(iterator != NULL){
-//			cout << iterator -> getTitulo() <<endl;
-//			delete iterator;
-//			iterator = pila->extraer();
-//		}
-//		delete pila;
-
 	return 0;
 }
