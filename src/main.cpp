d41e3389a748c676b6ee5e988d63d2de83f17abf
José Aquino <jose_aquino5@hotmail.com>
2016-07-25 12:14:13 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 // Author      : VMeta
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Library Management System in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
